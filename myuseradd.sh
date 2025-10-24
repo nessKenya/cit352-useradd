86b98d84b77d488daddc39eea6598e623d2c7220
nelson.muchonji@gmail.com <nelson.muchonji@gmail.com>
2025-10-24 11:07:39 +0300

    Stages 2 and 3 of the Bash Scripting Project

--- myuseradd.sh
+++ myuseradd.sh
@@ -1,40 +1,4 @@
 #!/bin/bash
-##Author: Nelson Muchonji Bifwoli
-#
-
-function print_usage() {
-	echo "Usage: myuseradd.sh -a <login> <passwd> <shell> - add a user account"
-	echo "myuseradd.sh -d <login>  -remove a user account"
-	echo "myuseradd.sh -h         - display this usage message"
-}
-
-function delete_user() {
-	echo "delete_user"
-}
-
-function add_user() {
-	echo "add_user"
-}
-
-function parse_command_options () {
-	if [ $# -eq 0 ]; then
-		print_usage
-		exit 1
-	fi
-
-	case "$1" in
-		-h)
-			print_usage
-			;;
-		-d)
-			delete_user "2"
-			;;
-		-a)
-			add_user "$2" "$3" "$4"
-			;;
-		*)
-
-			echo "ERROR : Invalid option $1"
 
 #Author: Nelson Muchonji Bifwoli
 #
@@ -51,6 +15,12 @@ add_user() {
 local username=$1
 local password=$2
 local shell=$3
+
+#Check if all arguments are provided
+if [[ -z "$username" || -z "$password" || -z "$shell" ]]; then
+	echo "Usage: ./myuseradd.sh -a <username> <password> <shell>"
+	exit 1
+fi
 
 #Check if user already exists
 if id "$username" &>/dev/null; then
